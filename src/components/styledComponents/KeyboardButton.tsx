b04c43566b5514faec7adbde4d61864e766105ea
kennizen <prachurjyagogoi123@gmail.com>
2023-05-29 12:18:33 +0530

    ui change

--- src/components/styledComponents/KeyboardButton.tsx
+++ src/components/styledComponents/KeyboardButton.tsx
@@ -7,7 +7,7 @@ const KeyboardButton = ({ children, onClick }: IPorps) => {
     return (
         <button
             onClick={onClick}
-            className="px-4 py-2 bg-slate-300 font-bold text-center rounded-md uppercase flex-1 min-w-max active:bg-slate-400 outline-none"
+            className="px-4 py-2 bg-slate-300 font-bold text-center rounded-md uppercase flex-1 min-w-max active:bg-slate-400 outline-none hover:ring-2 ring-slate-400"
         >
             {children}
         </button>
